3184c9c66804e2a7a7bca23ba6faf911fe428999
Lkad <eloony@163.com>
2017-08-09 18:42:43 +0800

    modified:   app/auth/__pycache__/views.cpython-34.pyc 	modified:   app/auth/views.py 	modified:   app/emails.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,8 @@ from flask_login import login_user,login_required
 from . import auth
 from ..models import User,db
 from .forms import LoginForm,RegistrationForm
+from ..emails import send_email
+from flask_login import current_user
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -34,7 +36,41 @@ def register():
         user = User(email=form.email.data,username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('you can now login')
+        token = user.generate_confirmation_token()
+        send_mail(user.email,'Confirm Your Account','auth/email/confirm',usr=user,token=token)
+        flash('A confirmation email has been sent to you by email')
+
+
         return redirect(url_for('auth.login'))
     return render_template('auth/login.html',form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('you have confirmed your account,Thanks!')
+    else:
+        flash('the confirmation link is invalid or has expired.')
+        return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth' and request.dendpoint !='static':
+        return render_template('auth/unconfirmed.html')
+@auth.route('/unconfirmed')
+def unconfigred():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.genrate_confirmation_token()
+    send_email(current_user.email,'Confirm Your Account','auth/email/confirm',user=current_user,token=token)
+    flash('A new confirmation email has been sent to you bu email;')
+    return redirect(url_for('main.index'))
+
 
--- app/emails.py
+++ app/emails.py
@@ -6,8 +6,8 @@ def send_async_email(app,msg):
         mail.send(msg)
 
 def send_email(to,subject,template,**kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SENDER'],+subject,sender=app.config['FLASK_MAIL_SENDER'],recipients[to])
+    msg = Message(app.config['FLASKY_MAIL_SENDER'],+subject,sender=app.config['FLASK_MAIL_SENDER'],recipients=[to])
     msg.html=render_template(template+'.html',**kwargs)
-    thr = Thread(target=send_async_email,args=[app,msg]
+    thr = Thread(target=send_async_email,args=[app,msg])
     thr.start() 
     return thr
